b67499fa0fd6cf4fbdcb5a8cba50090d8d213a5f
cyan <18722611216@163.com>
2025-05-09 17:30:43 +0800

    初始提交

--- music-client/src/assets/data/header.js
+++ music-client/src/assets/data/header.js
@@ -1,28 +1,28 @@
-//左侧导航栏
+// 左侧导航栏
 const navMsg = [
-    {name: '首页',path: '/'},
-    {name: '歌单',path: '/song-list'},
-    {name: '歌手',path: '/singer'},
-    {name: '我的音乐',path: '/my-music'}
+  {name: '首页', path: '/'},
+  {name: '歌单', path: '/song-list'},
+  {name: '歌手', path: '/singer'},
+  {name: '我的音乐', path: '/my-music'}
 ]
 
-//右侧导航栏
+// 右侧导航栏
 const loginMsg = [
-    {name: '登录',path: '/login-in'},
-    {name: '注册',path: '/sign-up'}
+  {name: '登录', path: '/login-in'},
+  {name: '注册', path: '/sign-up'}
 ]
 
-//用户下拉菜单
+// 用户下拉菜单
 const menuList = [
-    {name: '音乐人申请',path: '/musician-apply', show: 'Listener'},  // 仅听众可见
-    {name: '音乐管理',path: '/music-manage', show: 'Musician'},      // 仅音乐人可见
-    {name: '系统管理',path: '/admin', show: 'Admin'},                // 仅管理员可见
-    {name: '设置',path: '/setting'},
-    {name: '退出',path: 0}
+  {name: '音乐人申请', path: '/musician-apply', show: 'Listener'}, // 仅听众可见
+  {name: '音乐管理', path: '/music-manage', show: 'Musician'}, // 仅音乐人可见
+  {name: '系统管理', path: '/admin', show: 'Admin'}, // 仅管理员可见
+  {name: '设置', path: '/setting'},
+  {name: '退出', path: 0}
 ]
 
 export {
-    navMsg,
-    loginMsg,
-    menuList
+  navMsg,
+  loginMsg,
+  menuList
 }
